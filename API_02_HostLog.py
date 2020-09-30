2d6ee40e031c8534cb62e81ef2ea5dd3da700653
Craig Macivor <CMacivor@ad.pendantautomation.com>
2020-09-30 14:53:13 -0400

    add the db log function, fix new container

--- API_02_HostLog.py
+++ API_02_HostLog.py
@@ -14,6 +14,9 @@ m_type must not be longer than 15 characters
 
 import requests
 import python_config
+import mysql.connector
+from datetime import datetime
+import time
 
 # Deployment Variables
 
@@ -24,21 +27,54 @@ api = "/api/hostlogs/store"
 
 
 def log(auth, domain, source, m_type, message):
-    loggingConfig = python_config.read_logging_config()
-    auth = loggingConfig.get('auth')
-    domain = loggingConfig.get('domain')
-    api = loggingConfig.get("api")
-    url = domain + api
+    dbLog(source, m_type, message)
 
-    parsedMessage = str(message).replace("b", "").replace("\\", "").replace("\x02", "").replace("\x03", "")
+    # loggingConfig = python_config.read_logging_config()
+    # auth = loggingConfig.get('auth')
+    # domain = loggingConfig.get('domain')
+    # api = loggingConfig.get("api")
+    # url = domain + api
 
-    data = {"source": source, "type": m_type, "message": parsedMessage}
+    # parsedMessage = str(message).replace("b", "").replace("\\", "").replace("\x02", "").replace("\x03", "")
 
-    response = requests.post(url, json=data, allow_redirects=False)
+    # data = {"source": source, "type": m_type, "message": parsedMessage}
 
-    test = response
+    # response = requests.post(url, json=data, allow_redirects=False)
 
-  
+    # test = response
+
+
+def dbLog(source, m_typ, message):
+    config = python_config.read_db_config()
+    host = config.get('host')
+    user = config.get('user')
+    database = config.get('wcsdatabase')
+    password = config.get('password')
+
+    try:
+        connection = mysql.connector.connect(
+                host= host, 
+                user= user, 
+                database= database, 
+                password= password 
+            )
+
+        cursor = connection.cursor()
+
+        currentTimeStamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        insertLogSql = ("INSERT INTO host_logs "
+                                "(source, type, message, created_at, updated_at) "
+                                "VALUES (%s, %s, %s, %s, %s)")
+        
+        parsedMessage = str(message).replace("b", "").replace("\\", "").replace("\x02", "").replace("\x03", "")
+        
+        newLog = (source, m_typ, parsedMessage, currentTimeStamp, currentTimeStamp)
+
+        cursor.execute(insertLogSql, newLog)
+        connection.commit()
+    except Exception as e:
+        print(e)
 
 
 #test = log("Basic YWE6YQ==", "https://dev.pendantautomation.com", "WXS to Host", "Dock Scan Log Request", "Log Scan KD3101017-001")
