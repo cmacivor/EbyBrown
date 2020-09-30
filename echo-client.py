70b3bdddbde61b5a736985ab5c3eff44b2ffbdbf
Craig Macivor <CMacivor@ad.pendantautomation.com>
2020-09-30 15:33:59 -0400

    fix route complete

--- echo-client.py
+++ echo-client.py
@@ -9,7 +9,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     #s.sendall(b'\x0200013|ADDCONTA|307604|1123|SC307604112-006|307604112|006003|SPLITCASE|113205|20\x03') # New Container
     #s.sendall(b'\x0200013|CONTCOMP|SC307604112-006|307604112|1\x03') #Container Complete
     #s.sendall(b'\x0200013|ASGNCOMP|307604112\x03') #Assignment Complete
-    s.sendall(b'\x0200013|ORDRCOMP|307604|1123\x03') #Order Complete
+    #s.sendall(b'\x0200013|ORDRCOMP|307604|1123\x03') #Order Complete
+    s.sendall(b'\x0200013|ROUTCOMP|307604\x03') # Route Complete
     data = s.recv(1024)
 
 print("Received", repr(data))
