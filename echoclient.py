fc7fad5f8667c8e3852c3d5b0c1d2c390b48bec8
Craig Macivor <CMacivor@ad.pendantautomation.com>
2020-10-14 14:58:29 -0400

    get this working

--- echoclient.py
+++ echoclient.py
@@ -1,16 +1,68 @@
 import socket
 from time import sleep
+import mysql.connector
+from mysql.connector import (connection)
+import python_config
+import Eby_GlobalConstants as GlobalConstants
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+
+# HOST = '127.0.0.1'  # The server's hostname or IP address
+# PORT = 65432        # The port used by the server
+
+HOST = '10.22.56.11'  # The server's hostname or IP address
+PORT = 9998        # The port used by the server
+
+def getLogs():
+    try:
+        config = python_config.read_db_config()
+        host = config.get('host')
+        user = config.get('user')
+        database = config.get('database')
+        #wcsDatabase = config.get('wcsdatabase')
+        password = config.get('password')
+
+        connection = mysql.connector.connect(
+            host= host, 
+            user= user, 
+            database= database, 
+            password= password 
+        )
+
+        cursor = connection.cursor()
+
+        SQL = "select * from host_logs where type = 'ASGNCOMP' || type = 'ORDRCOMP' || type = 'CONTCOMP' || type = 'ROUTCOMP' || type = 'ADDCONTA'"
+
+        cursor.execute(SQL)
+
+        result = cursor.fetchall()
+
+        return result
+    except Exception as e:
+        print(e)
+
+        
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    hostLogsArr = []
+    hostLogs = getLogs()
+    for log in hostLogs:
+        message = log[3]
+        concatMsg = GlobalConstants.StartTransmissionCharacter + message + GlobalConstants.EndTransmissionCharacter
+        encodedMessage = concatMsg.encode('ascii')
+        hostLogsArr.append(encodedMessage)
+
     s.connect((HOST, PORT))
+
     while True:
-        s.sendall(b'\x027|KEEPALIV\x03')
-        data = s.recv(1024)
-        print(data)
-        sleep(3)
+        for msg in hostLogsArr:
+            
+            #s.sendall(b'\x027|KEEPALIV\x03')
+            s.sendall(msg)
+
+            data = s.recv(1024)
+            print(data)
+            sleep(1)
 
   
     
