a448844eef68345ea17bd57467f266b1b43ee595
Craig Macivor <CMacivor@ad.pendantautomation.com>
2020-10-14 10:42:45 -0400

    simulate continuous sending

--- echoclient.py
+++ echoclient.py
@@ -6,22 +6,25 @@ PORT = 65432        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
+    while True:
+        s.sendall(b'\x027|KEEPALIV\x03')
+        data = s.recv(1024)
+        print(data)
+        sleep(3)
 
-    s.sendall(b'\x027|KEEPALIV\x03')
-    data = s.recv(1024)
-    print(data)
+  
     
-    #sleep(10)
+    # #sleep(10)
 
-    s.sendall(b'\x027|KEEPALIV\x03')
-    data1 = s.recv(1024)
-    print(data1)
+    # s.sendall(b'\x027|KEEPALIV\x03')
+    # data1 = s.recv(1024)
+    # print(data1)
 
-    #sleep(10)
+    # #sleep(10)
 
-    s.sendall(b'\x027|KEEPALIV\x03')
-    data2 = s.recv(1024)
-    print(data2)
+    # s.sendall(b'\x027|KEEPALIV\x03')
+    # data2 = s.recv(1024)
+    # print(data2)
 
 
 #print('Received', repr(data))
