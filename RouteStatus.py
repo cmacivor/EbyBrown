37348ea44252805e400dd855eec66026b3de6f12
CMacivor <CMacivor@PRECISION-4JVDK>
2020-10-16 14:50:47 -0400

    load result into object array

--- RouteStatus.py
+++ RouteStatus.py
@@ -1,3 +1,9 @@
+import python_config
+import mysql.connector
+import traceback
+import sys
+import GlobalFunctions
+
 class route_status:
     def __init__(self, id, route, dockDoor, trailerNumber, priority, enable, status, date_at, created_at, updated_at, numberLines, existingRecordCount):
         self.ID = id
@@ -11,37 +17,3 @@ class route_status:
         self.CreatedAt = created_at
         self.UpdatedAt = updated_at
 
-    def getUnCompletedRouteStatuses(self):
-        try:
-            config = python_config.read_db_config()
-            host = config.get('host')
-            user = config.get('user')
-            #database = config.get('database')
-            wcsDatabase = config.get('wcsdatabase')
-            password = config.get('password')
-
-            connection = mysql.connector.connect(
-                host= host, 
-                user= user, 
-                database= wcsDatabase, 
-                password= password 
-            )
-
-            cursor = connection.cursor()
-
-            getRouteStatusesSQL = "select * from route_statuses where status != 'Complete' order by priority"
-
-            cursor.execute(getRouteStatusesSQL)
-
-            result = cursor.fetchall()
-
-            return result
-
-        except Exception as e:
-            print(e)
-            exc_type, exc_value, exc_traceback = sys.exc_info()
-            lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
-            exceptionMsg = exc_value.msg
-            exceptionDetails = ''.join('!! ' + line for line in lines)
-            
-            GlobalFunctions.logExceptionStackTrace(exceptionMsg, exceptionDetails) 
--- ShippingDashboard.py
+++ ShippingDashboard.py
@@ -4,9 +4,59 @@ import python_config
 import GlobalFunctions
 import sys
 import traceback
-import Eby_DatConverter
+import RouteStatus
 
 
+def main():
+    unCompletedRouteStatuses = getUnCompletedRouteStatuses()
+    test = unCompletedRouteStatuses
+    
+
+def getUnCompletedRouteStatuses():
+    try:
+        config = python_config.read_db_config()
+        host = config.get('host')
+        user = config.get('user')
+        #database = config.get('database')
+        wcsDatabase = config.get('wcsdatabase')
+        password = config.get('password')
+
+        connection = mysql.connector.connect(
+            host= host, 
+            user= user, 
+            database= wcsDatabase, 
+            password= password 
+        )
+
+        cursor = connection.cursor()
+
+        getRouteStatusesSQL = "select * from route_statuses where status != 'Complete' order by priority"
+
+        cursor.execute(getRouteStatusesSQL)
+
+        routeStatusObjList = []
+        result = cursor.fetchall()
+        for routeStatus in result:
+            routeStatusObj = RouteStatus.route_status(routeStatus[0], routeStatus[1], routeStatus[2], routeStatus[3], routeStatus[4], routeStatus[5], routeStatus[6], routeStatus[7], 
+                                                        routeStatus[8], routeStatus[9], 0, 0)
+            routeStatusObjList.append(routeStatusObj)
+
+        return routeStatusObjList
+
+    except Exception as e:
+        print(e)
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+        exceptionMsg = exc_value.msg
+        exceptionDetails = ''.join('!! ' + line for line in lines)
+        
+        GlobalFunctions.logExceptionStackTrace(exceptionMsg, exceptionDetails) 
+
+
+
+
+if __name__ == "__main__":
+    main()
 
 
 
