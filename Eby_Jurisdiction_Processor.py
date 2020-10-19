62054a9389ee86cac647ecdd72e978b931e80a32
CMacivor <CMacivor@PRECISION-4JVDK>
2020-10-19 14:53:16 -0400

    create the file

--- Eby_Jurisdiction_Processor.py
+++ Eby_Jurisdiction_Processor.py
@@ -1,15 +1,44 @@
 import GlobalFunctions
-import sys
+import sys, os
 import traceback
 import mysql.connector
 import python_config
+from pathlib import Path
 
 def process(containerId):
+    #get file paths
+    datFileConverterConfig = python_config.read_fileconverter_config()
+    #inputPath = datFileConverterConfig.get('input_path')
+    outputPath = datFileConverterConfig.get('output_path')
+    #inputProcessedPath = datFileConverterConfig.get('input_processed_path')
+    #outputProcessedPath = datFileConverterConfig.get('output_processed_path')
+    #fileDeleteInterval = datFileConverterConfig.get('file_delete_interval')
+
     #query the dat_master table against the containerId, get the jurisdiction and qty
     datFileRecord = getDatFileRecordByContainerId(containerId)
 
     #create the sub dat file contents
     fileContents = createFileContents(datFileRecord)
+
+    fileName = containerId.strip() + ".dat"
+
+    fullFilePath = outputPath + "\\" + fileName
+
+    #check if the file exists
+    #if fullFilePath.is_file():
+    if os.path.exists(fullFilePath):
+        #overwrite it
+        test = fullFilePath
+    else:
+        #create a new one
+        with open(fullFilePath, "w") as containerFile:
+            containerFile.write(fileContents)
+            print(containerFile.name + " created." )
+
+
+
+
+
 
     #create the sub dat file
 
