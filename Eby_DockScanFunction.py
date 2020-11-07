de5a980c36078b9a7d01f3834d06165142330863
RobWard83 <66741656+RobWard83@users.noreply.github.com>
2020-11-07 07:14:56 -0500

    Update Eby_DockScanFunction.py
    Refactored

--- Eby_DockScanFunction.py
+++ Eby_DockScanFunction.py
@@ -68,114 +68,91 @@ def dock_scan_control(door):
         print("Door " + str(door) + " = " + str(triggerBit))
 
     elif triggerBit == True:
-        try:
-            connection = mysql.connector.connect(
-                host= host, 
-                user= user, 
-                database= database, 
-                password= password 
-            )
-
-            cursor = connection.cursor()
         
-            print("Door " + str(door) + " = " + str(triggerBit))
+        
+        print("Door " + str(door) + " = " + str(triggerBit))
 
-            ret = comm.Read("DockDoorScanner" + door + ".TxTriggerID", datatype=INT)
-            TxTriggerID = ret.Value
-            print(TxTriggerID)
+        ret = comm.Read("DockDoorScanner" + door + ".TxTriggerID", datatype=INT)
+        TxTriggerID = ret.Value
+        print(TxTriggerID)
 
-            ret = comm.Read("DockDoorScanner" + door + ".TxMessage", datatype=STRING)
-            TxMessage = ret.Value[5:18]
-            print(TxMessage)
+        ret = comm.Read("DockDoorScanner" + door + ".TxMessage", datatype=STRING)
+        TxMessage = ret.Value[5:18]
+        print(TxMessage)
 
-            reason = ""
-            if "NOREAD" in TxMessage:
-                TxMessage = "No Read"
-                reason = "No Read"
-            elif "MULTIREAD" in TxMessage:
-                TxMessage = "Multi-Read"
-                reason = "Multi-Read"
+        reason = ""
+        if "NOREAD" in TxMessage:
+            TxMessage = "No Read"
+            reason = "No Read"
+        elif "MULTIREAD" in TxMessage:
+            TxMessage = "Multi-Read"
+            reason = "Multi-Read"
+        else:
+            pass
+
+        print(TxMessage)
+        
+        exists = "SELECT EXISTS (SELECT * FROM assignment.dat_master WHERE container_id=" + "'" + str(TxMessage) + "')"
+        cursor.execute(exists)
+        result = cursor.fetchone()
+        exists = result[0]
+        print(exists)
+
+        route = ""
+        stop = ""
+        if exists == 1:
+            
+            if TxMessage != "No Read" and TxMessage != "Multi-Read" and TxMessage != "":
+                # Mark in dat_master table as stop_scan
+                cursor.execute("UPDATE assignment.dat_master SET stop_scan=1 WHERE container_id=" + "'" + str(TxMessage) + "'")
+                connection.commit()
+
+                info = "SELECT route_no, stop_no FROM assignment.dat_master WHERE container_id=" + "'" + str(TxMessage)+ "'"
+                cursor.execute(info)
+                result = cursor.fetchall()                
+                route = str(result[0][0])
+                stop = str(result[0][1])
+                print(route)
+                print(stop)
+
             else:
                 pass
-
-            print(TxMessage)
             
-            exists = "SELECT EXISTS (SELECT * FROM assignment.dat_master WHERE container_id=" + "'" + str(TxMessage) + "')"
-            cursor.execute(exists)
-            result = cursor.fetchone()
-            exists = result[0]
-            print(exists)
-
-            route = ""
-            stop = ""
-            if exists == 1:
-                
-                if TxMessage != "No Read" and TxMessage != "Multi-Read" and TxMessage != "":
-                    # Mark in dat_master table as stop_scan
-                    cursor.execute("UPDATE assignment.dat_master SET stop_scan=1 WHERE container_id=" + "'" + str(TxMessage) + "'")
-                    connection.commit()
-
-                    info = "SELECT route_no, stop_no FROM assignment.dat_master WHERE container_id=" + "'" + str(TxMessage)+ "'"
-                    cursor.execute(info)
-                    result = cursor.fetchall()                
-                    route = str(result[0][0])
-                    stop = str(result[0][1])
-                    print(route)
-                    print(stop)
-
-                else:
-                    pass
-                
-            elif exists == 0 and TxMessage == "No Read":
-                reason = "No Read"
-            elif exists == 0 and TxMessage == "Multi-Read":
-                reason = "Multi-Read"
-            else:
-                reason = "Not in DB"
-            
-            currentTimeStamp = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-
-            
-            # Log Scan to dashboard_door_scans regardless of read type
-            cursor.execute("INSERT INTO wcs.dashboard_door_scans (door_id,barcode,route,stop,reason,created_at,updated_at) VALUES ("+str(door)+",'"+str(TxMessage)+"','"+route+"','"+stop+"','"+reason+"','"+currentTimeStamp+"','"+currentTimeStamp+"')")
-            connection.commit()
-
-            RxMessage = "Scan Logged"
-
-            # After scan is logged, write received and reset bit
-            comm.Write("DockDoorScanner" + door + ".RxMessage", RxMessage)
-            comm.Write("DockDoorScanner" + door + ".RxTriggerID", TxTriggerID)
-            comm.Write("DockDoorScanner" + door + ".TxTrigger", False)
-            #print("Scan Logged")
-            
-            return "Scan Logged"
+        elif exists == 0 and TxMessage == "No Read":
+            reason = "No Read"
+        elif exists == 0 and TxMessage == "Multi-Read":
+            reason = "Multi-Read"
+        else:
+            reason = "Not in DB"
         
-            # Execute Scan Reason Logic
-            # TODO Add Function here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            # pause = scanPause()
-            # print(pause)
+        currentTimeStamp = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
         
-        except Exception as e:
-            print(e)
-            
-            currentTimeStamp = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-            
-            # Log Scan to dashboard_door_scans regardless of read type
-            cursor.execute("INSERT INTO wcs.dashboard_door_scans (door_id,barcode,route,stop,reason,created_at,updated_at) VALUES ("+str(door)+",'"+str(TxMessage)+"','"+route+"','"+stop+"','"+reason+"','"+currentTimeStamp+"','"+currentTimeStamp+"')")
-            connection.commit()
-            
-            # After scan is logged, write received and reset bit
-            comm.Write("DockDoorScanner" + door + ".RxMessage", "NACK")
-            comm.Write("DockDoorScanner" + door + ".RxTriggerID", TxTriggerID)
-            comm.Write("DockDoorScanner" + door + ".TxTrigger", False)
-            print("Error")
-            
-            return "Error in Processing"
+        # Log Scan to dashboard_door_scans regardless of read type
+        cursor.execute("INSERT INTO wcs.dashboard_door_scans (door_id,barcode,route,stop,reason,created_at,updated_at) VALUES ("+str(door)+",'"+str(TxMessage)+"','"+route+"','"+stop+"','"+reason+"','"+currentTimeStamp+"','"+currentTimeStamp+"')")
+        connection.commit()
+
+        RxMessage = "Scan Logged"
+
+        # After scan is logged, write received and reset bit
+        comm.Write("DockDoorScanner" + door + ".RxMessage", RxMessage)
+        comm.Write("DockDoorScanner" + door + ".RxTriggerID", TxTriggerID)
+        comm.Write("DockDoorScanner" + door + ".TxTrigger", False)
+        #print("Scan Logged")
+        
+        return "Scan Logged"
+    
+        # Execute Scan Reason Logic
+        # TODO Add Function here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        # pause = scanPause()
+        # print(pause)
+
+        
+       
 
 
     else:
-        print("ValueError: Out of Range")
+        return "ValueError: Out of Range"
 
 
 
@@ -188,13 +165,28 @@ doors = [1, 2]
 while True:
     
     try:
+        
+        connection = mysql.connector.connect(
+                host= host, 
+                user= user, 
+                database= database, 
+                password= password 
+                )
+
+        cursor = connection.cursor()
+        
+        
         for door in doors:
             
             doorScan = dock_scan_control(door)
             print(doorScan)
             
+    
+    
     except Exception as e:
-        print(e)    
+        print(e) 
+        
+        
 
         with PLC() as comm:
             comm.IPAddress = plcIP
@@ -207,5 +199,5 @@ while True:
     
     
     
-atexit.register(cursor.close)
+
 atexit.register(connection.close())
