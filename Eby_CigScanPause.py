bab4c9fe293a630f9b65b7989a3e00f54f4f5269
RobWard83 <66741656+RobWard83@users.noreply.github.com>
2020-11-08 08:29:39 -0500

    Update Eby_CigScanPause.py
    First complete pass

--- Eby_CigScanPause.py
+++ Eby_CigScanPause.py
@@ -20,72 +20,156 @@ from datetime import datetime
 from pylogix import PLC
 import sys
 import atexit
+import Eby_01_Jurisdiction as jurisdiction
+import os  
 
-
-config = python_config.read_db_config()
-host = config.get('host')
-user = config.get('user')
-database = config.get('wcsdatabase')
-password = config.get('password')
-
-logging = python_config.read_logging_config()
-auth = logging.get('auth')
-domain = logging.get('domain')
-plcIP = "10.22.56.34"
-
-# Data Types
-STRUCT = 160
-BOOL = 193
-SINT = 194
-INT = 195
-DINT = 196
-LINT = 197
-USINT = 198
-UINT = 199
-UDINT = 200
-LWORD = 201
-REAL = 202
-LREAL = 203
-DWORD = 211
-STRING = 218
 
 
 
 
 
 def no_read(code):
-    #Stuff
-    test = test
+    #Barcode missing or otherwise unable to be read
+
+    enabled = "SELECT status FROM wcs.scan_reasons WHERE location='Cigarette Module' AND reason='No Read'"
+    cursor.execute(enabled)
+    result = cursor.fetchone()
+    enabled = int(result[0])
+    #print(enabled)
+
+    if enabled == 1:
+        if "no" in code.lower():
+            return True
+        else:
+            return False
+    else:
+        return False
     
 
 def multi_read(code):
-    #Stuff
-    test = test
+    #Two barcodes read at same time
+
+    enabled = "SELECT status FROM wcs.scan_reasons WHERE location='Cigarette Module' AND reason='Multi Read'"
+    cursor.execute(enabled)
+    result = cursor.fetchone()
+    enabled = int(result[0])
+    #print(enabled)
+    
+    if enabled == 1:
+        if "multi" in code.lower():
+            return True
+        else:
+            return False
+    else:
+        return False
     
 
 def code_not_found(code):
-    #Stuff
-    test = test
+    #barcode scanned doesn't exist in database (has to be the approved barcode symbology, containing the dash in the correct spot, correct number of characters, etc.)
+    
+    enabled = "SELECT status FROM wcs.scan_reasons WHERE location='Cigarette Module' AND reason='Barcode Not Found'"
+    cursor.execute(enabled)
+    result = cursor.fetchone()
+    enabled = int(result[0])
+    #print(enabled)
+    
+    if enabled == 1:
+        exists = "SELECT EXISTS (SELECT * FROM assignment.dat_master WHERE container_id=" + "'" + str(code) + "')"
+        cursor.execute(exists)
+        result = cursor.fetchone()
+        exists = result[0]
+        #print(exists)
+        
+        if exists == 1:
+            return True
+        else:
+            return False
+    else:
+        return False
     
     
 def stamp_not_required(code):
     #Stuff
-    test = test
+    return False
     
 
 def jurisdiction_not_found(code):
-    #Stuff
-    test = test
+    #barcode scanned doesn't exist in database (has to be the approved barcode symbology, containing the dash in the correct spot, correct number of characters, etc.)
+    
+    enabled = "SELECT status FROM wcs.scan_reasons WHERE location='Cigarette Module' AND reason='Jurisdiction Not Found'"
+    cursor.execute(enabled)
+    result = cursor.fetchone()
+    enabled = int(result[0])
+    #print(enabled)
+    
+    if enabled == 1:
+        exists = "SELECT jurisdiction FROM assignment.dat_master WHERE container_id=" + "'" + str(code) + "'"
+        cursor.execute(exists)
+        result = cursor.fetchone()
+        exists = result[0]
+        #print(exists)
+        
+        if exists.isspace() == True:
+            return True
+        else:
+            return False
+    else:
+        return False
     
 
 def jurisdiction_lane_not_configured(code):
-    #Stuff
-    test = test
+    # Jurisdiction read from file not configured to divert to any lane
+    
+    enabled = "SELECT status FROM wcs.scan_reasons WHERE location='Cigarette Module' AND reason='Jurisdiction Lane Not Configured'"
+    cursor.execute(enabled)
+    result = cursor.fetchone()
+    enabled = int(result[0])
+    #print(enabled)
+    
+        
+    if enabled == 1:
+        
+        juris = "SELECT jurisdiction FROM assignment.dat_master WHERE container_id=" + "'" + str(code) + "'"
+        cursor.execute(juris)
+        result = cursor.fetchone()
+        juris = result[0]
+        print(juris)
+        
+        ret = jurisdiction.lookup(auth, domain, str(juris))
+        httpCode = ret[0]
+        if httpCode == "200":
+            lane = ret[1]
+            
+            if lane == 1 or lane == 2 or lane == 3:
+                return True
+            else:
+                return False
+            
+        else:
+            return False
+    else:
+        return False
     
     
 def stamp_file_not_found(code):
-    #Stuff
-    test = test
+    # No file for stamping found on WCS server for United Slicone
+    
+    enabled = "SELECT status FROM wcs.scan_reasons WHERE location='Cigarette Module' AND reason='Stamp File Not Found'"
+    cursor.execute(enabled)
+    result = cursor.fetchone()
+    enabled = int(result[0])
+    #print(enabled)
+    
+    if enabled == 1:
+        if os.path.exists("D:\\Downloads\\UnitedSilicone\\" + str(code) + ".DAT") == True:
+            return False
+        else:
+            return True
+    else:
+        return False
+        
+    
+    
     
     
 
