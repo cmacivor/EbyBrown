ba672f9a684b61dc403f9b889fd0bac60b9c386c
Sadik <paul.dev422@gmail.com>
2020-11-10 15:30:23 -0500

    Eby_PickAreaCopyPaste added

---
+++ Eby_PickAreaCopyPaste.py
@@ -0,0 +1,61 @@
+import requests
+import time
+import requests
+import python_config
+import mysql.connector
+from datetime import datetime
+import sys
+import atexit
+import os
+
+
+config = python_config.read_db_config()
+host = config.get('host')
+user = config.get('user')
+wcsdatabase = config.get('wcsdatabase')
+database = config.get('database')
+password = config.get('password')
+
+def find_and_replace():
+    # scan assignment.dat_master for null pick_area and pick_group fields
+    try:
+
+        connection = mysql.connector.connect(
+                            host= host, 
+                            user= user, 
+                            database= database, 
+                            password= password 
+                        )
+        wcsconnection = mysql.connector.connect(
+                            host= host, 
+                            user= user, 
+                            database= wcsdatabase, 
+                            password= password 
+                        )
+
+        cursor = connection.cursor()
+        wcscursor = wcsconnection.cursor()
+        # WHERE pick_area IS NULL
+        sql = "SELECT pick_code FROM assignment.dat_master WHERE pick_area IS NULL AND pick_group IS NULL"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        for item in result:
+            if item[0] is not None:
+                pickCode = item[0][3:]
+                sql = "SELECT `pick_area`, `group` FROM wcs.pick_areas WHERE code = "+"'"+ pickCode + "'"
+                wcscursor.execute(sql)
+                result = wcscursor.fetchone()
+                if result is not None and result[0] is not None:
+                    sql = "UPDATE assignment.dat_master SET pick_area="+"'"+ result[0] + "', pick_group="+"'"+ result[1] + "' WHERE id="++"'"+ str(item[0]) + "'"
+                    cursor.execute(sql)
+                    connection.commit()
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        connection.close()
+        wcscursor.close()
+        wcsconnection.close()
+
+find_and_replace()
+
