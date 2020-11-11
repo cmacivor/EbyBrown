60426c26ab0210543c1dd4667d093692a3d88a03
Sadik <paul.dev422@gmail.com>
2020-11-11 15:26:36 -0500

    Assignment File page Status column updater added

---
+++ Eby_AssignmentStatus.py
@@ -0,0 +1,28 @@
+import Mysql_Connection
+import time
+
+connection = Mysql_Connection.get()
+cursor = connection.cursor()
+
+def change_status(id, status):
+    sql = "UPDATE assignment.dat_master SET status = "+"'"+ status +"'"+" WHERE id="+str(id)
+    cursor.execute(sql)
+    connection.commit()
+
+
+def update_assignment_status():
+    sql = "SELECT c_comp, stop_scan, id FROM assignment.dat_master WHERE status <> 'Shipped' OR status IS NULL"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    for item in result:
+        if item[0] == 0 and item[1] == 0:
+            change_status(item[2], 'Pending')
+        elif item[0] == 1 and item[1] == 0:
+            change_status(item[2], 'Complete')
+        elif item[0] == 1 and item[1] == 1:
+            change_status(item[2], 'Shipped')
+    return 'assignment status updated successfully.'
+        
+while True:
+    print(update_assignment_status())
+    time.sleep(5)
