9299ddd7a2e93c094ad13f84a3b767b5f042e6da
Sadik <paul.dev422@gmail.com>
2020-11-16 23:21:30 +0600

    Eby_RemainingToScan calculation

--- Eby_RemainingToScan.py
+++ Eby_RemainingToScan.py
@@ -1,56 +1,36 @@
 """
-Author: Robert J. Ward
+Author: Sadik
 Changelog:
--- Version: 1.0 Robert J. Ward
-    --- Initial Release
+-- Version: 1.0
+-- Initial Release
 
 """
 
 
 import time
 import requests
-import python_config
-import mysql.connector
 from datetime import datetime
 import sys
 import atexit
+import Mysql_Connection
 
-
-
-config = python_config.read_db_config()
-host = config.get('host')
-user = config.get('user')
-database = config.get('wcsdatabase')
-password = config.get('password')
-
-logging = python_config.read_logging_config()
-auth = logging.get('auth')
-domain = logging.get('domain')
-plcIP = "10.22.56.34"
-
-
-
+connection = Mysql_Connection.get()
+cursor = connection.cursor()
 
 def remaining_to_scan(route, date):
     
-    records = "SELECT id FROM assignment.dat_master WHERE status='Pending' AND count_flag <> 1"
-    cursor.execute(records)
-    records = cursor.fetchall() 
-        
-    for r in records:
-        pick_code = "SELECT pick_code FROM assignment.dat_master WHERE id=" + "'" + str(r[0]) + "'"
-        cursor.execute(pick_code)
-        pick_code = cursor.fetchone()[0]
-        print(pick_code)
-        
-        flag = "SELECT count_flag FROM wcs.pick_areas WHERE code=" + "'" + str(pick_code) + "'"
-        cursor.execute(flag)
-        flag = cursor.fetchone()[0]
-        print(flag)
-        flag = str(flag)
-        
-        if flag == "1":
-            cursor.execute("UPDATE assignment.dat_master SET count_flag=1 WHERE id=" +str(r[0])+";")
-    
-    connection.commit()
+    sql = "SELECT id FROM assignment.dat_master WHERE route_no="+ str(route) +" AND date='"+ str(date) +"' AND count_flag = 1"
+    cursor.execute(sql)
+    stops = []
+    eligable_counts = {}
+    eligable_counts[route] = len(cursor.fetchall())
 
+    sql = "SELECT stop_no, count(*) as `count` FROM dat_master WHERE route_no = 101 GROUP BY stop_no ORDER BY stop_no DESC"
+    cursor.execute(sql)
+    records = cursor.fetchall()
+
+    for r in records:
+        stops.append(int(r[0]))
+        eligable_counts[int(r[0])] = r[1]
+
+    return stops, eligable_counts
