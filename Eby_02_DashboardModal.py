c80f587f073c49f332652226d7d979dde9aabecd
RobWard83 <66741656+RobWard83@users.noreply.github.com>
2020-11-20 01:09:14 -0500

    Create Eby_02_DashboardModal.py

---
+++ Eby_02_DashboardModal.py
@@ -0,0 +1,47 @@
+"""
+Author: Robert J. Ward
+
+--Version: 1.0
+    --- Initial Release
+
+"""
+
+
+
+# Imports
+
+import requests
+import python_config
+from datetime import datetime
+import time
+
+# Deployment Variables
+
+domain = "http://10.22.56.11/"
+api = "api/popup-notifications/store"
+auth = "Basic YWE6YQ=="
+
+
+def pop_up(a_message, a_color, a_backgroud_color, a_expire, a_screen_number):
+    
+    url = domain + api
+    
+    json = {"message" : a_message,
+            "color" : a_color,
+            "background_color" : a_backgroud_color,
+            "expire" : a_expire,
+            "screen_number" : a_screen_number
+            }
+    
+    request = requests.post(url,
+                            headers={"Authorization": auth, "Content-Type": "application/json", "Accept": "application/json"},
+                            json=json
+                            )
+    
+    data = request.json()
+    
+    print(data)
+    
+    return data["id"]
+
+#print(pop_up("100 - 951<br>NO DOCK<br>PICS", "#E9EDF5", "#4287F5", "20", "1"))
